87e9e7dd328326a71b8aa1e86eb13f4916f13a7e
lekyvong09 <lekyvong09@gmail.com>
2021-10-20 21:21:49 +0700

    AOP - @Before

---
+++ src/main/java/com/ray/springmvc/aspect/LoggingAspect.java
@@ -0,0 +1,33 @@
+package com.ray.springmvc.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    // setup Logger
+    private static final Logger myLogger = LogManager.getLogger(LoggingAspect.class);
+
+    @Pointcut("execution(* com.ray.springmvc.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.ray.springmvc.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("execution(* com.ray.springmvc.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("forControllerPackage() || forDaoPackage() || forServicePackage()")
+    private void forLoggingAspect() {}
+
+    @Before("forLoggingAspect()")
+    public void beforeLogging(JoinPoint theJoinPoint) {
+        myLogger.info("===> in @Before advice: calling method: " + theJoinPoint.getSignature().toShortString());
+    }
+}
