a7ece4582dfb25ba5955ce87c3c0adc92db9e676
lekyvong09 <lekyvong09@gmail.com>
2021-10-27 19:11:54 +0700

    Spring Security - create userDAO

---
+++ src/main/java/com/ray/springmvc/dao/UserDAO.java
@@ -0,0 +1,7 @@
+package com.ray.springmvc.dao;
+
+import com.ray.springmvc.entity.User;
+
+public interface UserDAO {
+    User findByUserName(String username);
+}
---
+++ src/main/java/com/ray/springmvc/dao/UserDAOImpl.java
@@ -0,0 +1,23 @@
+package com.ray.springmvc.dao;
+
+import com.ray.springmvc.entity.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserDAOImpl implements UserDAO{
+
+    private SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public User findByUserName(String username) {
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        return currentSession.get(User.class, username);
+    }
+}
