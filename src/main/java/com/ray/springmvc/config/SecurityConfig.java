2c3c2903e710148b6b6bade213320c5e7b251844
lekyvong09 <lekyvong09@gmail.com>
2021-10-27 19:35:38 +0700

    Spring Security - DB authentication

--- src/main/java/com/ray/springmvc/config/SecurityConfig.java
+++ src/main/java/com/ray/springmvc/config/SecurityConfig.java
@@ -1,29 +1,37 @@
 package com.ray.springmvc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("myUserDetailsService")
+    private UserDetailsService myUserDetailsService;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication().withUser(users
-                .username("ray")
-                .password("password")
-                .roles("EMPLOYEE","ADMIN"));
-
-        auth.inMemoryAuthentication().withUser(users
-                .username("tommy")
-                .password("password")
-                .roles("EMPLOYEE"));
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication().withUser(users
+//                .username("ray")
+//                .password("password")
+//                .roles("EMPLOYEE","ADMIN"));
+//
+//        auth.inMemoryAuthentication().withUser(users
+//                .username("tommy")
+//                .password("password")
+//                .roles("EMPLOYEE"));
+        auth.userDetailsService(myUserDetailsService);
     }
 
     @Override
--- src/main/java/com/ray/springmvc/service/MyUserDetailsService.java
+++ src/main/java/com/ray/springmvc/service/MyUserDetailsService.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ public class MyUserDetailsService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         com.ray.springmvc.entity.User user = userDAO.findByUserName(username);
